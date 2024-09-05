0d5a40d20ea446979b7ec7a398fc18f52016ec82
Stine  Fjeldsrud Karlsen <stinefjeldsrudkarlsen@MBP-von-Stine.home>
2024-09-05 20:30:25 +0200

    managed to get a good training and test accuracy! now I need the plot

--- musicgenres.py
+++ musicgenres.py
@@ -84,6 +84,9 @@ classical_loudness = classical_songs_array[:, 1]
 classical_liveness = classical_songs_array[:, 0]
 
 # Scatter the values in different colors
+plt.figure()
+plt.title('Loudness and Liveness')
+
 plt.scatter(pop_loudness, pop_liveness, color='red')
 plt.scatter(classical_loudness, classical_liveness, color='blue')
 
@@ -112,7 +115,7 @@ def log_loss(true, prediction):
 
 # function for calculating the accuracy of the training
 def accuracy(y_pred_acc, y_test_acc):
-    return np.sum(y_pred_acc==y_test_acc)/len(y_test)
+    return np.sum(y_pred_acc==y_test_acc)/len(y_test_acc)
 
 
 #Define weight and bias
@@ -120,8 +123,11 @@ weight = np.zeros(x_training.shape[1])
 bias = 0
 
 #define learning rate and iterations
-learning_rate = 0.0001 #changeable
-epochs = 10 #changable
+learning_rate = 0.001 #changeable
+epochs = 1
+
+pred_training_list = []
+log_loss_list = []
 
 # Loop through all iterations to optain the optimal value of m and c
 for iterations in range(epochs):
@@ -140,25 +146,61 @@ for iterations in range(epochs):
         weight -= learning_rate * sgd_weight
         bias -= learning_rate * sgd_bias
 
-        pred_training = sigmoid(np.dot(x_training, weight) + bias)
+        #Calculate log loss by calculating the predictions
+        pred_training = sigmoid(np.dot(x_training[i], weight) + bias)
         loss = log_loss(y_training, pred_training)
+        log_loss_list.append(loss)
+        pred_training_list.append(1 if pred_training >= 0.5 else 0)
+
         print(f'Epoch {iterations + 1}/{epochs}, Loss: {loss}')
 
+# Calculate the accuracy of the training test
+pred_training = np.array(pred_training_list)
+acc_training_set = accuracy(pred_training, y_training)
+
+print(f'The accuracy of the training set is : {acc_training_set}')
+
+
+# #Plot the log loss and accuracy of the training set
+# plt.figure()
+# plt.title('Training set')
+# plt.scatter(epochs, log_loss_list, color='pink')
+
+# # Set the labels for the plot
+# plt.xlabel('Epochs')
+# plt.ylabel('Log Loss')      
+# plt.grid(True)
+# plt.legend()
+
+# plt.show()
+
+#--------------------------------------------------
+# PROBLEM 2B
+# Test the trained machine
 
 # Calculate the predictions with the test set
-pred = []
+pred_test_list = []
 
 for i in range(len(x_test)):
     z = np.dot(weight, x_test[i]) + bias
     y_pred = sigmoid(z)
 
-    if np.any(y_pred >= 0.5):
-        pred.append(1)
+    if (y_pred >= 0.5):
+        pred_test_list.append(1)
     else:
-        pred.append(0)
+        pred_test_list.append(0)
 
-pred_test = np.array(pred)
+pred_test = np.array(pred_test_list)
 
 # Calculate the accuracy of the training
-acc = accuracy(pred, y_test)
-print(f'The accuracy of the training is:" {acc}')
+acc_test_set = accuracy(pred_test, y_test)
+print(f'The accuracy of the test set is: {acc_test_set}')
+
+
+#--------------------------------------------------
+# PROBLEM 2C
+
+
+#--------------------------------------------------
+# PROBLEM 3A
+
